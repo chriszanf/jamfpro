e59768eb40b36dcd9429233b0c93418bccd05e6f
Adam Codega <adamcodega@swipely.com>
2015-04-22 10:01:25 -0400

    Create check-chrome-users.sh

---
+++ check-chrome-users.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+loggedinuser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+ 
+chromestate=`cat  /Users/$loggedinuser/Library/Application\ Support/Google/Chrome/Local\ State | grep user_name`
+ 
+echo "<result>$chromestate</result>"
