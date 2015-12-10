c13cbafbfb28b2dc3913dbda28556d7021a654cc
Adam Codega <adamcodega@swipely.com>
2015-12-10 00:39:56 -0500

    Additions and fixes
    Improved the description text
    
    Added unzipping SelfService.tar.gz and clearing the quarantine from
    Self Service.app

--- brandSelfService.sh
+++ brandSelfService.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
-# 
+#
 # brandSelfService.sh
 #
 # Copy pre-made graphic files inside
 # of Self Service.app for branding purposes
-# 
-# Place this and Self Service.app in a folder
-# chmod +x this file
-# Place assets in a subfolder named icons
-# 
-# Files needed:
+#
+# HOW TO:
+# 1: Download Self Service from your JSS (SelfService.tar.gz)
+#
+# 2: Place this script and SelfService.tar.gz in a folder
+# chmod +x this script
+# Place assets (see Files Needed below) in a subfolder named icons
+#
+# 3: Run ./brandSelfService.sh from Terminal
+#
+# Files Needed:
 # Your replacement icon for the app, 256x256: /icons/appicon.icns
 # Your Retina replacement icon for the app, 512x512: /icons/appicon@2x.icns
 # Your replacement for the status area of SS, 84x84: /icons/logo-SelfService.tiff
-# 
+#
 # YOU MUST RUN THIS COMMAND WITH ELEVATED PRIVILEGES (sudo)
 #
 # For use with the JAMF Casper Suite
 #
 # Adam Codega, Swipely
 #
+
+# Unzip SelfService.tar.gz
+tar -zxvf SelfService.tar.gz
+
+# Clear the quarantine from Self Service.app
+xattr -r -d com.apple.quarantine Self\ Service.app
 
 # Copy the icon into place
 echo "Copying the icon file into place.."
@@ -44,7 +55,7 @@ chmod 744 Self\ Service.app/Contents/Info.plist
 
 # Rename the app itself
 echo "Renaming Self Service the app itself.."
-# Put your preferred app name at the end of this line in double quotes
+# Put your preferred app name at the end of the next line in double quotes
 mv Self\ Service.app "Swipely Service.app"
 
 echo
