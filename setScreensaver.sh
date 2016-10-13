d887060da59845ec96a97fd893cea6507f4c7274
Adam Codega <adamcodega@swipely.com>
2016-10-13 10:08:08 -0400

    Added full path of defaults
    Avoid some odd errors occurring at times

--- setScreensaver.sh
+++ setScreensaver.sh
@@ -8,8 +8,8 @@
 
 loggedinuser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 
-sudo -u "$loggedinuser" -i 'defaults -currentHost write com.apple.screensaver idleTime -int "600"'
+sudo -u "$loggedinuser" -i '/usr/bin/defaults -currentHost write com.apple.screensaver idleTime -int "600"'
 
-sudo -u "$loggedinuser" -i 'defaults -currentHost write com.apple.screensaver askForPassword -int "1"'
+sudo -u "$loggedinuser" -i '/usr/bin/defaults -currentHost write com.apple.screensaver askForPassword -int "1"'
 
-sudo -u "$loggedinuser" -i 'defaults -currentHost write com.apple.screensaver askForPasswordDelay -int "0"'
+sudo -u "$loggedinuser" -i '/usr/bin/defaults -currentHost write com.apple.screensaver askForPasswordDelay -int "0"'
