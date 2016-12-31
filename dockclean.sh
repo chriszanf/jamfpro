5ef5030bd86ac9f2af01b9e4258735bd920389f3
Adam Codega <adamcodega@swipely.com>
2016-12-31 09:02:41 -0500

    Support for dockutil 2.0.5

--- dockclean.sh
+++ dockclean.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Configure the dock for a new user
+
 DOCKUTIL=/usr/local/bin/dockutil
 
-$DOCKUTIL --remove all
+$DOCKUTIL --remove all --no-restart
 
 $DOCKUTIL --add '/Applications/Launchpad.app' --no-restart
 
@@ -11,5 +13,9 @@ $DOCKUTIL --add '/Applications/Google Chrome.app' --no-restart
 $DOCKUTIL --add '/Applications/System Preferences.app' --no-restart
 
 $DOCKUTIL --add '~/Downloads'
+
+sleep 2
+
+/usr/bin/killall Dock >/dev/null 2>&1
 
 exit 0
