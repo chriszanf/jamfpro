73492ec24de176f9442a1177429a62532d95b7cf
Adam Codega <adamcodega@swipely.com>
2017-01-07 09:10:32 -0500

    Clean up syntax and use Python snippet to determine logged in user

--- make-computername.sh
+++ make-computername.sh
@@ -3,31 +3,34 @@
 # make-computername.sh
 #
 # Get computer user's full name
-# And make a firstinitial-lastname version
+# And make a firstinitiallastname hostname
 # To set the computer name
 #
-# Adam Codega, Swipely
+# Adam Codega
 #
 
 # find the JAMF binary location
 jamfbinary='/usr/bin/which jamf'
 
+# find the user who's logged in
+user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+
 # get full name
-name=$(finger "`whoami` | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //'")
+name=$(finger "$user" | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //' )
 
 # get first name
-finitial="$(echo $name | head -c 1)"
-echo $finitial
+finitial="$(echo "$name" | head -c 1)"
+echo "$finitial"
 
 # clean for lastname
-ln="$(echo $name | cut -d \  -f 2)"
-echo $ln
+ln="$(echo "$name" | cut -d \  -f 2)"
+echo "$ln"
 
 # add first and last together
 fullname=($finitial$ln)
 
-# clean up un to have all lower case
-hostname=$(echo $fullname | awk '{print tolower($0)}')
+# clean up fullname to lower case
+hostname=$(echo "$fullname" | awk '{print tolower($0)}')
 
 # name the computer
-$jamfbinary setComputerName -name $hostname
+$jamfbinary setComputerName -name "$hostname"
